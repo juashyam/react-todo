0ffab16ca0165f43ac5f7e3e67b22ca2618a7653
Shyam Kumar <shyam@corra.com>
2020-03-29 20:12:36 +0530

    Add comments for firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,16 @@
 import firebase from 'firebase'
 
+/**
+ * To get the Firebase config, first go to the Firebase Homepage (https://console.firebase.google.com/) and click on 
+ * the Create New Project or the App Project Button. Also, make sure that you have logged into your google account.
+ * 
+ * In the Firebase project’s homepage, you will notice three circular icons. The first two will contain code 
+ * that will help you connect the firebase project to an iOS and Android app respectively, while the third 
+ * one will have code that will help you connect the project to a web app (which is what we want).
+ * 
+ * Click on it and copy the apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, 
+ * and appId values.
+ */
 const firebaseConfig = {
     apiKey: "XXXXXXXXXXXXXXXXXXXX",
     authDomain: "XXXXXXXXXXXXXXXXXXXX",
